8972cc274c55311b97af3d00f1b70aff39aeba8f
licerlee <licerlee@gmail.com>
2018-04-20 23:12:57 +0800

    due commit

--- ch03-01-build-simple-java/helloworld/run.sh
+++ ch03-01-build-simple-java/helloworld/run.sh
@@ -1,2 +1,2 @@
-docker image build . -t licerlee/hello-world-java:latest
-docker container run licerlee/hello-world-java:latest
+docker build . -t licerlee/hello-world-java:latest
+docker run licerlee/hello-world-java:latest
